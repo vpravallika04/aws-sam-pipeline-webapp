cc0ee051387f43bae63afdcccb7cacc965d059a6
vpravallika04 <pvalla@my.bridgeport.edu>
2023-11-30 11:48:30 -0500

    second commit

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "New message. second",
             # "location": ip.text.replace("\n", "")
         }),
     }
